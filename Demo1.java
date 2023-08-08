94f2fb57f28d34dcfab545e0b401590bea7036d3
isuranga <deshikaisu95@gmail.com>
2023-08-08 08:16:11 +0530

    7th commit

--- Demo1.java
+++ Demo1.java
@@ -93,9 +93,16 @@ public class Demo1{
 
         System.out.println("+----------------------------------------+");
         System.out.println("|                                          |");
-        System.out.printf("|  Name  : name.toUpperCase());
-        System.out.println(status);
-
+        System.out.println("|  Name  : " +name.toUpperCase()+"|");
+        System.out.println("|   Age : " +age +"years old" +  "|" );
+        System.out.println("|   Status : "+ status +    "|");
+        System.out.println("|   Total : " + total +     "Average :" + age +     "|");
+        System.out.println("+----------------------------------------+");
+        System.out.println("|   Subject     |   Marks   |   Status      |");
+        System.out.println("|   "+sub1+  "|"    +marks1+    "|  "+status1   +"|");
+        System.out.println("|   "+sub2+  "|"    +marks2+    "|  "+status2   +"|");
+        System.out.println("|   "+sub3+  "|"    +marks3+    "|  "+status3   +"|");
+        System.out.println("+----------------------------------------+");
 
 
     }
